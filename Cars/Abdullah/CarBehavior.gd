dcc7f98793a7a666e4919262f3a1b7e091671099
Fenguinn <salem.abdulla15@gmail.com>
2021-06-12 16:12:38 -0400

    Car Rotation Working

--- Cars/Abdullah/CarBehavior.gd
+++ Cars/Abdullah/CarBehavior.gd
@@ -22,11 +22,18 @@ func move_along_path(distance):
 	while path.size():
 		var distance_between_points = last_point.distance_to(path[0])
 		if distance <= distance_between_points:
+			var here = self.position
 			self.position = last_point.linear_interpolate(path[0], distance / distance_between_points)
+			var there = self.position
+			#dumb dumb time start
+			print ( (here-there) )
+			self.rotation = here.angle_to_point(there) - PI/2
+			#dumb dumb time begin
 			return
 		distance -= distance_between_points
 		last_point = path[0]
 		path.remove(0)
+		
 	self.position = last_point
 	set_process(false)
 
@@ -88,3 +95,9 @@ func _update_navigation_path(start_position, end_position):
 func _on_Car_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		emit_signal("clicked_on")
+
+
+func _on_Timer_timeout():
+	#if self.hasCollidedwithwall for longer than two seconds:
+		#delete self
+	$Timer.start(2)
